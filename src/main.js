a143724729d4a990db0ed0375a6e43f251116110
Ganesh Gunasegaran <me@itsgg.com>
2021-10-15 13:12:20 +0530

    Add bulma

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from "vue";
+
+// XXX: Load only the components we need
+import "../node_modules/bulma/css/bulma.min.css";
+
 import App from "./App.vue";
 
 createApp(App).mount("#app");
